889b362f6c729bc682bffcd8000afceb01f4b118
KeyanGootkin <KeyanGootkin@gmail.com>
2018-07-03 11:47:14 -0400

    adding garbage

--- dataenter.py
+++ dataenter.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 datafile = "C:/Users/Keyan/Desktop/Science/Data/GCS/cmedata.txt"
 table = pd.read_csv(datafile, sep = ':',header=0)
-print(table)
+;jlafsd;kjlaf;jkaw;krjafwkaw;a
