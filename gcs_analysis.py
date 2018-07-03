6bfdcfc9d3d6618e9eab017490f002ada3c554ac
KeyanGootkin <KeyanGootkin@gmail.com>
2018-07-03 13:57:49 -0400

    fixing common directory problem

--- gcs_analysis.py
+++ gcs_analysis.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
-datafile = "C:/Users/Keyan/Desktop/Science/Data/GCS/cmedata.txt"
+datafile = str(os.path.dirname(os.path.realpath(__file__)))+"/cmedata.txt"
+print(datafile)
 table = pd.read_csv(datafile, sep = ':',header=0)
+print()
