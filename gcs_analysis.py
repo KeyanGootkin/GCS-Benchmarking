fc1bfda1b2d77404c25f4184f152d073bb21e747
KeyanGootkin <KeyanGootkin@gmail.com>
2018-07-17 15:04:01 -0400

    changing txt file output, adding more analysis stuff

--- gcs_analysis.py
+++ gcs_analysis.py
@@ -5,16 +5,18 @@ import os
 import croissant as cr
 from glob import glob
 
-datdir = str(os.path.dirname(os.path.realpath(__file__))) + "/data/"
+datdir = str(os.path.dirname(os.path.realpath(__file__))) + "/data"
 datafolders = glob(datdir+'/*')
 datafiles = []
 for folder in datafolders:
     for file in glob(folder+'/*'):
         datafiles.append(file)
-
+cmes = []
 eUCLID = pd.DataFrame()
 for file in datafiles:
     average_df,start_t,start_h = cr.function_name(file)
     enlil_start_time = cr.find_cme_start(start_t,start_h,average_df["Velocity"])
     average_df["Time at 21.5"] = pd.Series([enlil_start_time])
     eUCLID = eUCLID.append(average_df)
+print(eUCLID)
+eUCLID.to_csv(str(os.path.dirname(os.path.realpath(__file__)))+"/eUCLID.txt")
