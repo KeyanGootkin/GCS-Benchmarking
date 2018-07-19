3159f85bbbf38f8669afb4400645382c280d69bc
Crawford <mcrawfo2@gs674-mashaair.ndc.nasa.gov>
2018-07-19 11:01:45 -0400

    Added cme_match.

---
+++ cme_match.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Given our current setup, directories must be a list with items in the format
+# "data/[acjkr]data".
+
+from glob import glob
+from re import search
+
+def cme_match(directories):
+
+    files, cmes = [], []
+    
+    for directory in directories:
+        files.extend(glob(directory + "/[0-9]WLRT_[1-2]???-??-??.rt"))
+    
+    for file in files:
+        cmes.append(file[-13:-3])
+        cmes = list(set(cmes))
+    
+    cmes.sort()
+    matches = {key: [] for key in cmes}
+
+    for key in matches:
+        matches[key].append([file for file in files if search(key,file)])
+ 
+    return matches
