53620fbfee8d479129735db38865c54cad53a98e
Crawford <mcrawfo2@gs674-mashaair.ndc.nasa.gov>
2018-07-19 14:38:00 -0400

    13% smarter than original cme_match and equally fancy but in a different way.

--- cme_match.py
+++ cme_match.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Given our current setup, directories must be a list with items in the format
-# "data/[acjkr]data".
+# Given our current setup, directories must be in the format "data/[acjkr]data".
 
 from glob import glob
-from re import search
 
-def cme_match(directories):
+def cme_match(*directories):
 
     files, cmes, matches = [], [], []
-    
-    for directory in directories:
-        files.extend(glob(directory + "/[0-9]WLRT_[1-2]???-??-??.rt"))
-    
+
+    if len(directories) == 0:
+        files.extend(glob("data/[acjkr]data/[0-9]WLRT_[1-2]???-??-??.rt"))
+    else:
+        for directory in directories:
+            files.extend(glob(directory + "/[0-9]WLRT_[1-2]???-??-??.rt"))
+        
     for file in files:
-        cmes.append(file[-13:-3] + file[-19:-18])
-        cmes = list(set(cmes))
+        cmes.append(file[-19:])
 
+    cmes = list(set(cmes))
     cmes.sort()
-
+    
     for cme in cmes:
-        matches.append([file for file in files if search(cme[-1:] + "WLRT_" + 
-        cme[:9],file)])
-         
+        matches.append([file for file in files if cme in file])
+        
     return matches
-
-print(cme_match(["data/rdata","data/cdata"]))
