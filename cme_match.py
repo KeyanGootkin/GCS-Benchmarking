6f5d7bba034317531a2d7ccfe8359f81393a4118
Crawford <mcrawfo2@gs674-mashaair.ndc.nasa.gov>
2018-07-19 13:45:23 -0400

    cme_match now 12% smarter but less fancy.

--- cme_match.py
+++ cme_match.py
@@ -9,19 +9,21 @@ from re import search
 
 def cme_match(directories):
 
-    files, cmes = [], []
+    files, cmes, matches = [], [], []
     
     for directory in directories:
         files.extend(glob(directory + "/[0-9]WLRT_[1-2]???-??-??.rt"))
     
     for file in files:
-        cmes.append(file[-13:-3] + "cme" + file[-19:-18])
+        cmes.append(file[-13:-3] + file[-19:-18])
         cmes = list(set(cmes))
 
     cmes.sort()
-    matches = {key: [] for key in cmes}
 
-    for key in matches:
-        matches[key].extend([file for file in files if search(key[-1:] + "WLRT_" + key[:9],file)])
- 
+    for cme in cmes:
+        matches.append([file for file in files if search(cme[-1:] + "WLRT_" + 
+        cme[:9],file)])
+         
     return matches
+
+print(cme_match(["data/rdata","data/cdata"]))
