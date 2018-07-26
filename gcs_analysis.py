4f3f699345b25711147556e8251661f33509b2c5
KeyanGootkin <KeyanGootkin@gmail.com>
2018-07-26 10:58:16 -0400

    yadayadayada

--- gcs_analysis.py
+++ gcs_analysis.py
@@ -11,7 +11,7 @@ datdir = str(os.path.dirname(os.path.realpath(__file__))) + "/data"
 all_cmes = cr.cme_match()
 cmedf = pd.DataFrame()
 all_cmesdf = pd.DataFrame()
-
+print(all_cmes)
 for cme in all_cmes:
     for mf in cme:
         measurement = pd.read_csv(mf, names=["Time", "Lon", "Lat", "ROT", "Height", "Ratio", "Half Angle"],
@@ -19,15 +19,38 @@ for cme in all_cmes:
         ave_measurement = measurement.mean()
         times_list = cr.cme_times(measurement["Time"])
         velocity = cr.cme_line_fit(times_list, measurement["Height"], return_slope = True)
-        ave_measurement = pd.DataFrame({"Time":measurement["Time"][len(measurement["Time"])-1], 'Lon': ave_measurement['Lon'], 'Lat': ave_measurement['Lat'], 'ROT': ave_measurement['ROT'],
+        ave_measurement = pd.DataFrame({"Time":measurement["Time"][len(measurement["Time"])-1], 'Lon': cr.cr2sh(measurement["Time"][len(measurement["Time"])-1],ave_measurement['Lon']), 'Lat': ave_measurement['Lat'], 'ROT': ave_measurement['ROT'],
                                     "Velocity":velocity, 'Ratio': ave_measurement['Ratio'], 'Half Angle': ave_measurement['Half Angle']}, index=[0])
         all_cmesdf = all_cmesdf.append(ave_measurement,ignore_index=True)
     cmedf = cmedf.append(ave_measurement,ignore_index=True)
 cmedf.to_csv(str(os.path.dirname(os.path.realpath(__file__))) +'/eUCLID.txt',sep = ":")
 print(all_cmesdf)
+print(cmedf)
+
+dates = []
+for t in cmedf["Time"]:
+    dates.append(t[:10])
+
 normarray = []
 for name in all_cmesdf.columns:
-    if all_cmesdf[name].dtypes != 'object' and name != 'ROT':
+    if all_cmesdf[name].dtypes != 'object' and name != "ROT":
+
+        new_norm = pd.Series()
+        for base_time in dates:
+            print(base_time)
+            temp_storage = []
+            for ind_time,x in zip(all_cmesdf["Time"],all_cmesdf[name]):
+                if base_time[:10] == ind_time[:10]:
+                    temp_storage.append(x)
+            norms = (np.array(temp_storage)-np.mean(np.array(temp_storage)))/np.mean(np.array(temp_storage))
+            normarray.append(norms)
+            plt.hist(norms)
+            plt.title(name +' '+ base_time)
+            plt.show()
+
+    """
+
+    if all_cmesdf[name].dtypes != 'object' and name == "Half Angle":
 
         new_norm = pd.Series()
         for base_time in cmedf["Time"]:
@@ -38,7 +61,7 @@ for name in all_cmesdf.columns:
             norms = (np.array(temp_storage)-np.mean(np.array(temp_storage)))/np.mean(np.array(temp_storage))
 
             normarray.append(norms)
-
+"""
 
 all_measurements = []
 for name in normarray:
@@ -46,18 +69,20 @@ for name in normarray:
         all_measurements.append(i)
 #normdf.hist()
 #plt.show()
-print(all_measurements)
+
 
 all_measurements=(np.array(all_measurements)*100)
-
+plt.figure(figsize=[10,10])
 plt.hist(all_measurements,bins=30)
-plt.title("Measurement Distrubution", size=23, fontname='Times New Roman')
+plt.title("CME Measurement Distrubution", size=20, fontname='Times New Roman')
 # Labels the x and y axis.
-plt.gca().set_xlabel("Percent Error", size=17, fontname='Times New Roman')
+plt.gca().set_xlabel("Percent Spread in Measurement", size=17, fontname='Times New Roman')
 plt.ylabel("Frequency", size=17, fontname='Times New Roman')
 # Makes the axis look nice (Thank you Cayenne)
 plt.xticks(fontsize=15, fontname='Times New Roman', color='darkslategrey')
 plt.yticks(fontsize=15, fontname='Times New Roman', color='darkslategrey')
 # Creates a grid on the plot to make it more readable
+plt.savefig("C:/Users/Keyan/Desktop/cmespread.png",overwrite=True)
+
 plt.show()
 print(np.std(all_measurements))
